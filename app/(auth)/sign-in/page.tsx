67e14187b1b8e704964b2ce7b1170941b5c79247
Nikhil Pratap Singh <123050049+localghost09@users.noreply.github.com>
2026-02-08 21:59:22 +0530

    changing the previous path location

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 
 import { Suspense } from 'react'
-import AuthForm from '@/components/AuthForm'
+import AuthForm from '@/components/auth/AuthForm'
 
 function SignInContent() {
   return <AuthForm type='sign-in' />
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import AuthForm from '@/components/AuthForm'
+import AuthForm from '@/components/auth/AuthForm'
 
 function SignUpContent() {
   return <AuthForm type='sign-up' />
